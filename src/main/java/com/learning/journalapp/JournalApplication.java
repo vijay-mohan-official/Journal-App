1caac9eb4a508c1a695b167abc6262c561637ae0
vijay-mohan-official <vijaymohanofficial@gmail.com>
2024-10-07 21:41:37 +0530

    Journal App Initialized package

---
+++ src/main/java/com/learning/journalapp/JournalApplication.java
@@ -0,0 +1,15 @@
+package com.learning.journalapp;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JournalApplication.class, args);
+    }
+
+
+}
