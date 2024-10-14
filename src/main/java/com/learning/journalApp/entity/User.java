3357a28279621ddc68485b96a110f5f9252abbd3
vijay-mohan-official <vijaymohanofficial@gmail.com>
2024-10-14 21:30:39 +0530

    JUnit Learning Beginning

--- src/main/java/com/learning/journalApp/entity/User.java
+++ src/main/java/com/learning/journalApp/entity/User.java
@@ -1,5 +1,6 @@
 package com.learning.journalApp.entity;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
 import org.bson.types.ObjectId;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Document(collection = "users")
 @Data
+@Builder
 public class User {
 
     @Id
