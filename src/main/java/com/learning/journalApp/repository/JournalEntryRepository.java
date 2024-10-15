6b942524bad1bd794ca92d4b08a95076701808f7
vijaymohan <vijay.mohan@ultsglobal.com>
2024-10-15 17:13:26 +0530

    Profiling

--- src/main/java/com/learning/journalApp/repository/JournalEntryRepository.java
+++ src/main/java/com/learning/journalApp/repository/JournalEntryRepository.java
@@ -4,7 +4,6 @@ import com.learning.journalApp.entity.JournalEntry;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-
 public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
 
 }
