d1bf622c2ec41f6dcae2f12519ae45a8eff53784
vijaymohan <vijay.mohan@ultsglobal.com>
2024-10-16 17:29:52 +0530

    Commenting tests

--- src/test/java/com/learning/journalApp/service/UserDetailsServiceImplTests.java
+++ src/test/java/com/learning/journalApp/service/UserDetailsServiceImplTests.java
@@ -1,42 +1,42 @@
-package com.learning.journalApp.service;
-
-import com.learning.journalApp.entity.User;
-import com.learning.journalApp.repository.UserRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collections;
-
-import static org.mockito.Mockito.when;
-
-//@SpringBootTest annotation will be used if we are playing with Application Context
-public class UserDetailsServiceImplTests {
-
-    //@Autowired will be used if we are playing with Application Context
-    @InjectMocks
-    private UserDetailsServiceImpl userDetailsService;
-
-    //Use @MockBean when playing with Application Context
-    @Mock
-    private UserRepository userRepository;
-
-    //Need to initialize all mocks as we are no longer playing with Application Context
-    @BeforeEach
-    void setUp(){
-        MockitoAnnotations.initMocks(this);
-    }
-
-    @Test
-    void loadUserByUserNameTest(){
-        when(userRepository.findByUserName(ArgumentMatchers.anyString())).thenReturn(User.builder().userName("vijay").password("vijay").roles(Collections.singletonList("USER")).build());
-        UserDetails userDetails = userDetailsService.loadUserByUsername("vijay");
-        Assertions.assertNotNull(userDetails);
-    }
-
-}
+//package com.learning.journalApp.service;
+//
+//import com.learning.journalApp.entity.User;
+//import com.learning.journalApp.repository.UserRepository;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.ArgumentMatchers;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.MockitoAnnotations;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import java.util.Collections;
+//
+//import static org.mockito.Mockito.when;
+//
+////@SpringBootTest annotation will be used if we are playing with Application Context
+//public class UserDetailsServiceImplTests {
+//
+//    //@Autowired will be used if we are playing with Application Context
+//    @InjectMocks
+//    private UserDetailsServiceImpl userDetailsService;
+//
+//    //Use @MockBean when playing with Application Context
+//    @Mock
+//    private UserRepository userRepository;
+//
+//    //Need to initialize all mocks as we are no longer playing with Application Context
+//    @BeforeEach
+//    void setUp(){
+//        MockitoAnnotations.initMocks(this);
+//    }
+//
+//    @Test
+//    void loadUserByUserNameTest(){
+//        when(userRepository.findByUserName(ArgumentMatchers.anyString())).thenReturn(User.builder().userName("vijay").password("vijay").roles(Collections.singletonList("USER")).build());
+//        UserDetails userDetails = userDetailsService.loadUserByUsername("vijay");
+//        Assertions.assertNotNull(userDetails);
+//    }
+//
+//}
--- src/test/java/com/learning/journalApp/service/UserServiceTest.java
+++ src/test/java/com/learning/journalApp/service/UserServiceTest.java
@@ -1,56 +1,56 @@
-package com.learning.journalApp.service;
-
-import com.learning.journalApp.entity.User;
-import com.learning.journalApp.repository.UserRepository;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-public class UserServiceTest {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private UserService userService;
-
-    @Test
-    public void testFindByUserName(){
-        assertNotNull(userRepository.findByUserName("admin"));
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {
-            "admin",
-            "vijay"
-    })
-    public void testFindByUserNames(String name){
-        assertNotNull(userRepository.findByUserName(name));
-    }
-
-    @Disabled
-    @ParameterizedTest
-    @CsvSource({
-            "1,1,2",
-            "2,4,6",
-            "3,6,10"
-    })
-    public void testCsvSource(int a, int b, int expected){
-        assertEquals(expected,a+b);
-    }
-
-    @Disabled
-    @ParameterizedTest
-    @ArgumentsSource(UserArgumentsProvider.class)
-    public void testArgumentsSource(User user){
-        assertTrue(userService.saveNewUser(user));
-    }
-}
+//package com.learning.journalApp.service;
+//
+//import com.learning.journalApp.entity.User;
+//import com.learning.journalApp.repository.UserRepository;
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.ArgumentsSource;
+//import org.junit.jupiter.params.provider.CsvSource;
+//import org.junit.jupiter.params.provider.ValueSource;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//public class UserServiceTest {
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    private UserService userService;
+//
+//    @Test
+//    public void testFindByUserName(){
+//        assertNotNull(userRepository.findByUserName("admin"));
+//    }
+//
+//    @ParameterizedTest
+//    @ValueSource(strings = {
+//            "admin",
+//            "vijay"
+//    })
+//    public void testFindByUserNames(String name){
+//        assertNotNull(userRepository.findByUserName(name));
+//    }
+//
+//    @Disabled
+//    @ParameterizedTest
+//    @CsvSource({
+//            "1,1,2",
+//            "2,4,6",
+//            "3,6,10"
+//    })
+//    public void testCsvSource(int a, int b, int expected){
+//        assertEquals(expected,a+b);
+//    }
+//
+//    @Disabled
+//    @ParameterizedTest
+//    @ArgumentsSource(UserArgumentsProvider.class)
+//    public void testArgumentsSource(User user){
+//        assertTrue(userService.saveNewUser(user));
+//    }
+//}
