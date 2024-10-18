0c374333d15c5a93fb08ec8f736cd2561c0b7893
vijay-mohan-official <vijaymohanofficial@gmail.com>
2024-10-18 20:15:37 +0530

    Sentiment analysis

---
+++ src/test/java/com/learning/journalApp/schedular/UserSchedularTests.java
@@ -0,0 +1,18 @@
+package com.learning.journalApp.schedular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserSchedularTests {
+
+    @Autowired
+    private UserScheduler userScheduler;
+
+    @Test
+    public void testSentimentAnalysis(){
+        userScheduler.fetchUsersAndSendEmail();
+    }
+
+}
